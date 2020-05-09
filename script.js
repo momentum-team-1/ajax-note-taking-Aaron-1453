bc273d49cb2812340ae022f1bd5a307c8af7b6bb
AaronOBrien1453 <aatobrien@gmail.com>
2020-05-09 15:22:48 -0400

    added unique id and title to notes, notes are going to api

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
+// Gernerates a unique id
+// https://stackoverflow.com/questions/36884815/how-to-generate-unique-id-in-javascript
+function id() {
+  var ALPHABET =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var ID_LENGTH = 8;
+  var rtn = "";
+  for (var i = 0; i < ID_LENGTH; i++) {
+    rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+  }
+  return rtn;
+}
+
 let noteForm = document.querySelector("#note-form");
 let notes = document.querySelector(".notes");
 
 noteForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  let noteTextInput = document.querySelector("#note-text");
-  let noteText = noteTextInput.value;
+  const noteTextInput = document.querySelector("#note-text");
+  const noteTitleInput = document.querySelector("#note-title");
+  const noteText = noteTextInput.value;
+  const noteTitle = noteTitleInput.value;
   noteTextInput.value = "";
-  createNewNote(noteText);
+  createNewNote(noteText, noteTitle);
+  //   renderNote();
 });
 
-function createNewNote(noteText) {
+function createNewNote(noteText, noteTitle) {
   fetch("http://localhost:3000/notes", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      item: noteText,
+      id: id(),
+      title: noteTitle,
+      body: noteText,
       created: moment().format(),
     }),
   })
@@ -24,21 +42,25 @@ function createNewNote(noteText) {
     .then(() => renderNote());
 }
 
-function renderNote(noteList) {
-  noteList.innerHTML = "";
+function renderNotes() {
   fetch("http://localhost:3000/notes", {
     method: "GET",
   })
-    .then((response) => {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      let list = document.createElement("ul");
-      list.id = "item-list";
-      for (let item of data) {
+    .then((response) => response.json())
+    .then((notes) => {
+      // for (d of data)
+      for (note of notes) {
+        return note;
       }
     });
 }
 
-renderNote();
+// .then(function (data) {
+//   console.log(data)
+//   let list = document.createElement("ul")
+//   list.id = "item-list"
+//   for (let item of data) {
+//   }
+// });
+
+renderNotes();
